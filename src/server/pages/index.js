bd6e178f690b326bba074b2877bfbbdc91c2f2c2
Dima Elemelkya <elemelkya@MacBook-Pro-DimaV.local>
2020-11-26 21:22:33 +0500

    top

--- src/server/pages/index.js
+++ src/server/pages/index.js
@@ -62,7 +62,7 @@ module.exports = (client) => new Promise((resolve) => {
                 Bots.push(bot);
             }
         });
-        if(req.user.id == '585766846268047370') {
+        if(req.user.id == '585766846268047370' || '578936810936467459') {
             res.redirect('https://github.com/ElemelkyaDima')
         }
         res.render("Index.ejs", { bot: req.bot, user: (req.user || null), bots: _.chunk(Bots, 4) });
