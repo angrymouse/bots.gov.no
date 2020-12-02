cee250116d12130c0c6a5b8a3a6dcdff1edc498e
Dmitry Homenko <71642531+ElemelkyaDima@users.noreply.github.com>
2020-12-02 08:26:29 -0800

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -48,7 +48,6 @@ module.exports = (client) => new Promise((resolve) => {
     app.get("/", async (req, res) => {
         const FetchedBots = await client.database.Bots.findAll({
             where: { isApproved: true },
-            limit : 5000,
             order: [
                 ['totalUpvotes', 'DESC'],
             ],
@@ -59,7 +58,7 @@ module.exports = (client) => new Promise((resolve) => {
             if(Bot) {
                 bot.tag = `${Bot.username}`;
                 bot.avatar = Bot.avatar;
-                bot.upvotes = client.database.Upvotes.get(`${bot.botID}_upvotes_${new Date().toISOString().slice(0, 1000)}`) || 0;
+                bot.upvotes = client.database.Upvotes.get(`${bot.botID}_upvotes_${new Date().toISOString().slice(0, 999)}`) || 0;
                 Bots.push(bot);
             }
         });
