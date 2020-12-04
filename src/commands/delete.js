42f32d56e393fcfd2f953fe1d4168812e73bcd3c
root <root@instance-1.us-central1-a.c.host-294116.internal>
2020-12-04 11:13:01 +0000

    ok

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -4,6 +4,6 @@ module.exports = {
     name: "deletebot",
     aliases: ["del"],
     run: async (client, message, args) => {
-            message.channel.send(`https://top-bots.top/${args}/delete`);
+            message.channel.send(`https://top-bots.top/bots/${args}/delete`);
     }
 }
